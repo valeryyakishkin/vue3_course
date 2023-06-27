3e4936adec59a2ea695167b53d0b142301f14159
valeryyakishkin <velyakishkin@gmail.com>
2023-06-27 22:49:36 +0300

    [2]: App component

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>Кол-во лайков</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+    };
+  },
+};
+</script>
+
+<style></style>
