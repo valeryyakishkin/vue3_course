a43a147a425e2beedb78687d6824b1f2509df04e
valeryyakishkin <velyakishkin@gmail.com>
2023-06-28 00:10:05 +0300

    [10]: Decomposition

---
+++ src/components/PostForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <input
+      v-bind:value="title"
+      @input="title = $event.target.value"
+      class="input"
+      type="text"
+      placeholder="Название"
+    />
+    <input
+      v-bind:value="body"
+      @input="body = $event.target.value"
+      class="input"
+      type="text"
+      placeholder="Описание"
+    />
+    <button class="btn">Создать</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="post" v-for="post in posts">
+    <div><strong>Название:</strong> {{ post.title }}</div>
+    <div><strong>Описание:</strong> {{ post.body }}</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+</style>
