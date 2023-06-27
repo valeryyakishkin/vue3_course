f6c43cc0b25e691f1aafca5fc970e929c3786cf0
valeryyakishkin <velyakishkin@gmail.com>
2023-06-27 23:53:58 +0300

    [8]: Bilateral binding

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,21 @@
   <div class="app">
     <form>
       <h4>Создание поста</h4>
-      <input class="input" type="text" placeholder="Название" />
-      <input class="input" type="text" placeholder="Описание" />
-      <button class="btn">Создать</button>
+      <input
+        v-bind:value="title"
+        @input="title = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Название"
+      />
+      <input
+        v-bind:value="body"
+        @input="body = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Описание"
+      />
+      <button class="btn" @click="createPost">Создать</button>
     </form>
     <div class="post" v-for="post in posts">
       <div><strong>Название:</strong> {{ post.title }}</div>
@@ -23,9 +35,20 @@ export default {
         { id: 3, title: "JavaScript 3", body: "Описание поста 3" },
         { id: 4, title: "JavaScript 4", body: "Описание поста 4" },
       ],
+      title: "",
+      body: "",
     };
   },
-  methods: {},
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      };
+      this.posts.push(newPost);
+    },
+  },
 };
 </script>
 
