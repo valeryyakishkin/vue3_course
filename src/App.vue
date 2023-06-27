f5d98c7a0e20db2adcb0ba1d03ebe6fab408e9ac
valeryyakishkin <velyakishkin@gmail.com>
2023-06-27 23:38:37 +0300

    [7]: Creating a new post

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
+  <div class="app">
+    <form>
+      <h4>Создание поста</h4>
+      <input class="input" type="text" placeholder="Название" />
+      <input class="input" type="text" placeholder="Описание" />
+      <button class="btn">Создать</button>
+    </form>
     <div class="post" v-for="post in posts">
       <div><strong>Название:</strong> {{ post.title }}</div>
       <div><strong>Описание:</strong> {{ post.body }}</div>
@@ -30,9 +36,34 @@ export default {
   box-sizing: border-box;
 }
 
+.app {
+  padding: 20px;
+}
+
 .post {
   padding: 15px;
   border: 2px solid teal;
   margin-top: 15px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
 }
 </style>
