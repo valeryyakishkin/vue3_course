b1c626c706771b3ba8db8da384a989f66066de44
valeryyakishkin <velyakishkin@gmail.com>
2023-07-07 23:07:34 +0300

    [31]: VUE ROUTER. Pagination

---
+++ src/router/router.js
@@ -0,0 +1,26 @@
+import About from "@/pages/About";
+import Main from "@/pages/Main";
+import UserPage from "@/pages/UserPage";
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    component: Main,
+  },
+  {
+    path: "/users",
+    component: UserPage,
+  },
+  {
+    path: "/about",
+    component: About,
+  },
+];
+
+const router = createRouter({
+  routes,
+  history: createWebHistory(process.env.BASE_URL),
+});
+
+export default router;
