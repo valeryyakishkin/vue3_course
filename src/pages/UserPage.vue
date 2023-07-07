513a89f1b2e3455cb2f03c5dc3491b21970ab93d
valeryyakishkin <velyakishkin@gmail.com>
2023-07-08 00:23:16 +0300

    [33]: Creating custom directives

--- src/pages/UserPage.vue
+++ src/pages/UserPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Страница с постами</h1>
-    <my-input v-model="searchQuery" placeholder="Поиск...." />
+    <my-input v-focus v-model="searchQuery" placeholder="Поиск...." />
     <div class="app__btns">
       <my-button @click="showDialog">Создать пользователя</my-button>
       <my-select v-model="selectedSort" :options="sortOptions" />
@@ -15,7 +15,7 @@
       v-if="!isPostsLoading"
     />
     <div v-else>Идёт загрузка...</div>
-    <div ref="observer" class="observer"></div>
+    <div v-intersection="loadMorePosts" class="observer"></div>
     <!-- <div class="page__wrapper">
       <div
         v-for="pageNumber in totalPages"
@@ -124,18 +124,18 @@ export default {
   mounted() {
     this.fetchPosts();
     console.log(this.$refs.observer);
-    const options = {
-      rootMargin: "0px",
-      threshold: 1.0,
-    };
+    // const options = {
+    //   rootMargin: "0px",
+    //   threshold: 1.0,
+    // };
 
-    const callback = (entries, observer) => {
-      if (entries[0].isIntersecting && this.page < this.totalPages) {
-        this.loadMorePosts();
-      }
-    };
-    const observer = new IntersectionObserver(callback, options);
-    observer.observe(this.$refs.observer);
+    // const callback = (entries, observer) => {
+    //   if (entries[0].isIntersecting && this.page < this.totalPages) {
+    //     this.loadMorePosts();
+    //   }
+    // };
+    // const observer = new IntersectionObserver(callback, options);
+    // observer.observe(this.$refs.observer);
   },
   computed: {
     sortedPosts() {
